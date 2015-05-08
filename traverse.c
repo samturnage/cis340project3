cde4ee626b5eb9e614ae5d40ee2842d390ca6141
Samuel Turnage <samturnage4@yahoo.com>
2015-05-08 17:56:45 -0400

    Update traverse.c

--- traverse.c
+++ traverse.c
@@ -1,185 +1,180 @@
+Skip to content
+ This repository
+Explore
+Gist
+Blog
+Help
+@samturnage samturnage
+ 
+ Unwatch 2
+  Star 1
+  Fork 0
+samturnage/cis340project4
+ branch: master  cis340project4/traverse.txt
+@DOWU10DOWU10 5 days ago Create traverse.txt
+1 contributor
+RawBlameHistory     159 lines (139 sloc)  4.296 kb
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <stdint.h>
+
+/* define a directory entry in the Root directory */
+struct direntry {
+  char name[8];
+  char ext[3];
+  char attribute;
+  char resv[10];	
+  uint8_t timeUpdated[2];  /* updated time */
+  uint8_t dateUpdated[2];  /* updated date */
+  char startCluster[2];
+  uint8_t size[4];
+};
 
 
+/* the properties of a root directory entry */
+#define	SLOT_EMPTY	    0x00
+#define	SLOT_DELETED	0xe5
+#define	ATTR_VOLUME  	0x08		
+#define	ATTR_DIRECTORY	0x10
+#define	ATTR_READONLY	0x01		
+#define	ATTR_HIDDEN	    0x02		
+#define	ATTR_SYSTEM	    0x04		
+#define	ATTR_ARCHIVE	0x20
 
-unsigned int convertHexToDec(unsigned char fileInfo[], short highestIndex, short numBytes, unsigned int * newValue)
-{
-//2 hex digits make up 1 character
-    short power = numBytes*2 - 1;
-    *newValue = 0;
-    unsigned int upperHex, lowerHex, decValue = 0;
-//for loops
-    short i,z;
-//go backwards because we are using little endian
-    for(z = highestIndex; z > (highestIndex - numBytes); --z)
-    {
-        decValue = (unsigned int)fileInfo[z];
-        if (decValue == 0)
-        {
-            power = power - 2;
-            continue;
-        }
-//upper bits
-        upperHex = (decValue/16);
-//lower bits
-        if (decValue > 15)
-            lowerHex = (decValue%16);
-        else
-            lowerHex = decValue;
-        for (i = power; i > 0; --i)
-        {
-            upperHex = upperHex*16;
-            if ((i - 1) > 0)
-                lowerHex = lowerHex*16;
-        }
-        *newValue = *newValue + upperHex + lowerHex;
-        power = power - 2;
+/* a helper function to display the root directory entry */
+static void displayEntry(struct direntry *de, int flag) {
+  /* construct the full name for the file or directory */
+  char fullName[14];
+  int i, k;
+
+  memset(fullName, 0, sizeof(fullName));
+
+  fullName[0] = '/';
+  k = 1;
+  for (i = 0; i < 8; i++) {
+    if (de->name[i] != ' ') {
+      fullName[k++] = de->name[i];
+    } else {
+      break;
     }
-//return the value to the location specified
-    return *newValue;
+  }
+  for (i = 0; i < 3; i++) {
+    if (de->ext[i] != ' ') {
+      if (i == 0) {
+        fullName[k++] = '.';
+      }
+      fullName[k++] = de->ext[i];
+    } else {
+      break;
+    }
+  }
+
+  if (flag) { /* detailed format */
+    
+    char attr[5];
+    int hour, minute, second;
+    int year, month, day;
+    uint16_t temp;
+    uint32_t size;
+
+    /* construct the attribute column */
+    attr[0] = (de->attribute & ATTR_READONLY) ? 'R' : '-';
+    attr[1] = (de->attribute & ATTR_ARCHIVE)  ? 'A' : '-';
+    attr[2] = (de->attribute & ATTR_SYSTEM)   ? 'S' : '-';
+    attr[3] = (de->attribute & ATTR_HIDDEN)   ? 'H' : '-';
+    attr[4] = '\0';
+
+    /* construct the time part */
+    temp = (de->timeUpdated[1] << 8) | de->timeUpdated[0];
+    second = ((temp) & 0x1f) * 2;
+    minute = (temp >> 5) & 0x3f;
+    hour = (temp >> 11) & 0x1f;
+
+    /* construct the date part */
+    temp = (de->dateUpdated[1] << 8) | de->dateUpdated[0];
+    day = (temp) & 0x1f;
+    month = (temp >> 5) & 0xf;
+    year = ((temp >> 9) & 0x7f) + 1980;
+
+    /* construct the file size */
+    size = (de->size[3] << 24) | (de->size[2] << 16) |
+           (de->size[1] << 8)  | (de->size[0]);
+
+    if ((de->attribute & ATTR_DIRECTORY) != 0) {
+      printf("%s\t%02d/%02d/%04d %02d:%02d:%02d\t<DIR>\t\t%s\n", 
+        attr, month, day, year, hour, minute, second, fullName);
+    } else {
+      printf("%s\t%02d/%02d/%04d %02d:%02d:%02d\t%10d\t%s\n", 
+        attr, month, day, year, hour, minute, second, size, fullName);
+    }
+  } else { /* short format */
+    printf("%-15s", fullName);
+    if ((de->attribute & ATTR_DIRECTORY) != 0) { 
+      /* this is a directory */
+      printf("\t\t\t<DIR>");
+    }
+    printf("\n");
+  }
+
 }
 
+void traverse(int flag) {
+  char num_fat;
+  uint16_t size_fat;
+  uint16_t num_root_dir;
+  int i;
+  struct direntry de;
 
-void PrintMoreSectorInfo(unsigned int fileInfoDec[],unsigned  char fileInfo[])
-{
-      //File type is displayed first
-      char attributes[] = "-----";
-      //get the attributes
-      unsigned int attributeNum = fileInfoDec[11];
-      //vars for looping
-      //check for subdirectorys and archives
-      if (attributeNum > 15)
-      {
-          if (attributeNum < 32)//Subdirectory
-              attributes[0] = 'D';
-          else if (attributeNum < 48)//Archive
-              attributes[1] = 'A';
-      }
-      if ((attributeNum % 16) == 0)
-      {
-          attributeNum = (attributeNum % 16);
-          //read only
-          if ((attributeNum % 2) != 0)
-              attributes[2] = 'R';
-          //hidden
-          if (attributeNum == 2 || attributeNum == 3 || attributeNum == 6 || attributeNum == 7 || 
-                attributeNum == 10 || attributeNum == 11 || attributeNum >= 14) //if second-lowest bit is set
-              attributes[3] = 'H';
-          //sys file
-          if ((attributeNum > 3 && attributeNum < 8) || attributeNum > 11)
-              attributes[4] = 'S';
-      }
-      printf("%s ", attributes);
-      //DATE
-      unsigned int year = 1980,monthNum = 0,dayNum = 0,monthDay,yearOffset = 0;
-      //YEAR
-      yearOffset = (fileInfoDec[17]/2);
-      yearOffset = ((yearOffset/16)*10 + yearOffset%16);
-      year = year + yearOffset;
-      //MONTH
-      monthDay = fileInfoDec[16];
-      monthDay = (monthDay - (monthDay%32));
-      if (monthDay == 32 || monthDay == 96 || monthDay == 160 || monthDay == 224)monthNum++;
-      if (monthDay == 64 || monthDay == 96 || monthDay == 192 || monthDay == 224)monthNum = monthNum + 2;
-      if (monthDay > 128) monthNum = monthNum + 4;
-      if(fileInfoDec[17] % 2 != 0) monthNum = monthNum + 8;
-      //DAY
-      monthDay = fileInfoDec[16];
-      dayNum = (((monthDay/16)*10) + (monthDay%16));
-      if (dayNum > 32)
-          dayNum = dayNum % 32;
-      printf("%d/%d/%d \t", monthNum, dayNum, year);
-      //TIME
-      unsigned int hourMin = fileInfoDec[15];
-      unsigned int minSec = fileInfoDec[14];
-      //HOUR
-      hourMin = (hourMin/8);
-      printf("%02d:", hourMin);
-      //MINUTE
-      hourMin = fileInfoDec[15];
-      minSec = (hourMin%8)*8 + (minSec/32);
-      printf("%02d:", minSec);
-      //SECONDS
-      minSec = fileInfoDec[14];
-      minSec = (minSec%32);
-      printf("%02d \t", minSec);
-      //File Size
-      unsigned int newDecValue = 0;
-      newDecValue = convertHexToDec(fileInfo, 31, 4, &newDecValue);
-      printf("%d\t", newDecValue);
-      //First Sector
-      newDecValue = convertHexToDec(fileInfo, 27, 2, &newDecValue);
-      printf("%d\t", newDecValue);
+  if (flag) { /* detailed format */
+    /* print the table header */
+    printf("\t*****************************\n");
+    printf("\t** FILE ATTRIBUTE NOTATION **\n");
+    printf("\t**                         **\n");
+    printf("\t** R ------ READ ONLY FILE **\n");
+    printf("\t** S ------ SYSTEM FILE    **\n");
+    printf("\t** H ------ HIDDEN FILE    **\n");
+    printf("\t** A ------ ARCHIVE FILE   **\n");
+    printf("\t*****************************\n");
+  }
+
+  /* read the number of FATs and number of directory entries,
+   * and the number of blocks for each FAT */
+  lseek(fd, 16, SEEK_SET);
+  read(fd, &num_fat, 1);
+  read(fd, &num_root_dir, 2);
+  lseek(fd, 22, SEEK_SET);
+  read(fd, &size_fat, 2);
+
+  /* seek to the beginning of root directory */
+  lseek(fd, (1 + num_fat * size_fat) * 512, SEEK_SET);
+
+  /* now start to read the directory entries */
+  for (i = 0; i < num_root_dir; i++) {
+    memset(&de, 0, sizeof(de));
+    read(fd, &de, sizeof(de));
+    if (de.name[0] == SLOT_EMPTY) {
+      /* this slot has not been used, means the end of the directory */
+      break;
+    }
+    if ((uint8_t)de.name[0] == SLOT_DELETED) {
+      /* this is a deleted slot */
+      continue;
+    }
+    if ((de.attribute & ATTR_VOLUME) != 0) {
+      continue;
+    } else { /* otherwise print the entry */
+      displayEntry(&de, flag);
+    }
+  }
 }
 
 int main(int argc, char *argv[])
 {
-      short flag = 0;
-      if(argc > 1 && strcmp("-l",argv[1]) == 0)
-	flag = 1;
-      short sector,file,a;
-      unsigned char fileInfo[32] = {'\0'};
-      unsigned int fileInfoDec[32] = {'\0'};
-      if (flag == 1)
-      {
-          printf("*****************************\n");
-          printf("** FILE ATTRIBUTE NOTATION **\n");
-          printf("** **\n");
-          printf("** R ------ READ ONLY FILE **\n");
-          printf("** S ------ SYSTEM FILE **\n");
-          printf("** H ------ HIDDEN FILE **\n");
-          printf("** A ------ ARCHIVE FILE **\n");
-          printf("*****************************\n");
-          printf("ATTRIB\tDATE\t\tTIME\t\tSIZE\tSECTOR\tNAME");
-      }
-      //read through root sectors
-      for (sector = 19; sector < 33; ++sector)
-      {
-          lseek(3, 512*sector, SEEK_SET);
-          
-          //16 files per sector
-          for (file = 0; file < 16; ++file)
-          {
-              lseek(3, (512*sector + file*32), SEEK_SET);
-              //32 btyes for root sector
-              read(3, fileInfo, 32);
-              if (fileInfo[0] == '\0')
-                  continue;
-              //cast to integer
-              for (a = 0; a < 32; ++a)
-                  fileInfoDec[a] = (unsigned int)fileInfo[a];
-              //ignore
-              if (fileInfoDec[11] == 15)
-                  continue;
-              printf("\n/");
-              
-              //print more info if flag is checked
-              if (flag == 1)
-                  PrintMoreSectorInfo(fileInfoDec,fileInfo);
-              //directory
-              if (fileInfo[11] == 16)
-              {
-                  for (a = 0; a < 11; ++a)
-                      printf("%c", fileInfo[a]);
-                  printf("\t<DIR>");
-              }
-              //normal file
-              else
-              {
-                  for (a = 0; a < 11; ++a)
-                  {
-                      if(fileInfo[a]!=' ')printf("%c", fileInfo[a]);
-                      //print a dot for the file extension
-                      if (a == 7)
-                          printf(".");
-                  }
-              }
-          }
-      }
-      printf("\n");
-      return 0;
+	traverse(atoi(argv[1]));
+	return 1;
 }
